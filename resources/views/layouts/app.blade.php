b05980401fd0d7850907854c797250c5a64fc315
Sidiki-del <you@example.com>
2023-01-24 11:41:02 +0000

    Login Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -77,5 +80,7 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
