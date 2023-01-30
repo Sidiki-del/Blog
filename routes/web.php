802698a2a1b6eddb532b06a35801614d868235ea
Sidiki-del <you@example.com>
2023-01-30 15:45:42 +0000

    Create And Read Category

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,8 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
-    Route::get('dashboard', [App\Http\Controllers\Admin\AdminController::class, 'index']);
+    Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
+    Route::get('add_category', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
+    Route::post('add_category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
 });
