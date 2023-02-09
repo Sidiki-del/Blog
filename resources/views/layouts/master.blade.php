85118859be9b04cf46cb23e3c9a2efe369463d1b
Sidiki-del <you@example.com>
2023-02-09 10:28:03 +0000

    Summernote Added Succesfully

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,6 +17,11 @@
         <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
 
 
+        {{-- Summernote CSS LINKS --}}
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+
+
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
 
@@ -40,7 +45,17 @@
 
 </div>
      </div>
+     <script src=" {{ asset('assets/js/jquery-3.6.0.min.js') }}" ></script>
      <script src=" {{ asset('assets/js/bootstrap.bundle.min.js') }}" ></script>
         <script src=" {{ asset('assets/js/scripts.js') }}"></script>
+
+        {{-- Summernote JS LINK --}}
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+        <script>
+    $(document).ready(function() {
+        $("#mySummernote").summernote();
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
 </body>
 </html>
